572540d482833373b4994fde3e292e0acf745585
linoor <michal.pomaranski@gmail.com>
2016-04-17 19:59:58 +0200

    cart page

--- app/controllers/CartController.scala
+++ app/controllers/CartController.scala
@@ -1,6 +1,15 @@
 package controllers
 
-class CartController {
+import javax.inject._
+import play.api.mvc._
+
+@Singleton
+class CartController @Inject() extends Controller {
+
+  def show(id: Long) = Action {
+    Ok(views.html.cart())
+  }
+
   def add(id: Long) = play.mvc.Results.TODO
 
   def get(id: Long) = play.mvc.Results.TODO
